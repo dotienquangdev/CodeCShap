4ea9fd17a1fd707cc6f53398484ba9520a1c2a67
Nguyen Xuan Dung <134990068+GunDx2k4@users.noreply.github.com>
2024-10-03 09:46:21 +0700

    Add project files.

---
+++ DBConnection.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BTL
+{
+    public class DBParameter
+    {
+        public SqlParameter SqlParameter { get; set; }
+
+        public object Value { get; set; }
+
+        public bool IsIdentity { get; set; }
+    }
+
+    public class DBConnection
+    {
+        private string DataSource { get; set; }
+
+        public string DataBase { get; set; }
+
+        public DataSet DataSet => _dataSet;
+        public static DBConnection Instance => _instance;
+
+        private static DBConnection _instance = new DBConnection();
+
+        private DataSet _dataSet = new DataSet();
+
+        public SqlConnection CreateConnection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+            return new SqlConnection(constr);
+        }
+
+        public DataTable SelectDB(string table)
+        {
+            string sqlSelect = $"SELECT * FROM {table}";
+            using (SqlConnection cnn = CreateConnection())
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    try
+                    {
+                        da.SelectCommand = new SqlCommand(sqlSelect, cnn);
+                        cnn.Open();
+                        da.Fill(_dataSet, table);
+                        cnn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("Lỗi select DataBase : " + ex.Message);
+                    }
+
+                }
+            }
+            return _dataSet.Tables[table];
+        }
+
+        public bool InsertDB(string table,string nameProc,params DBParameter[] sqlParameters)
+        {
+            DataTable dt = _dataSet.Tables[table];
+            if (_dataSet.Tables[table] == null)
+            {
+                dt = SelectDB(table);
+            }
+
+            using (SqlConnection cnn = CreateConnection())
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    using (SqlCommand cmd = cnn.CreateCommand())
+                    {
+                        try
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.CommandText = nameProc;
+                            foreach (DBParameter p in sqlParameters)
+                            {
+                                if (p.IsIdentity) continue;
+                                cmd.Parameters.Add(p.SqlParameter);
+                            }
+                            da.InsertCommand = cmd;
+                            DataRow newRow = dt.NewRow();
+                            foreach (DBParameter p in sqlParameters)
+                            {
+                                if (p.IsIdentity)
+                                {
+                                    newRow[0] = dt.Rows.Count + 1;
+                                    continue;
+                                }
+                                newRow[p.SqlParameter.SourceColumn] = p.Value;
+                            }
+                            dt.Rows.Add(newRow);
+                            cnn.Open();
+                            int i = da.Update(_dataSet, table);
+                            cnn.Close();
+                            return i > 0;
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception("Lỗi insert DataBase : " + ex.Message);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BTL
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.DataSource = DBConnection.Instance.SelectDB("tblKhachHang");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DBConnection.Instance.InsertDB("tblKhachHang", "sp_ThemKhachHang",
+                new DBParameter
+                {
+                    IsIdentity = true,
+                },
+                new DBParameter
+                {
+                    SqlParameter = new SqlParameter("@sHoTen", SqlDbType.NVarChar, 100, "sHoTen"),
+                    Value = "test"
+                },
+                new DBParameter
+                {
+                    SqlParameter = new SqlParameter("@sSoDienThoai", SqlDbType.NVarChar, 15, "sSoDienThoai"),
+                    Value = "test"
+                },
+                new DBParameter
+                {
+                    SqlParameter = new SqlParameter("@sDiaChi", SqlDbType.NVarChar, 255, "sDiaChi"),
+                    Value = "test"
+                },
+                new DBParameter
+                {
+                    SqlParameter = new SqlParameter("@sEmail", SqlDbType.NVarChar, 100, "sEmail"),
+                    Value = "test"
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
